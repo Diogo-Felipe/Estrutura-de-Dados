9d732e01dc28cc41ec4fa01cce79004794444a26
Diogo Felipe <dlcfelipp@gmail.com>
2020-02-21 09:11:30 -0300

    Inicialização do projeto + criação de um ArrayList

---
+++ src/ED/ArrayList01.java
@@ -0,0 +1,18 @@
+package ED;
+
+import java.util.*;
+
+public class ArrayList01 {
+
+	public static void main(String[] args) {
+		
+		//Criar o ArrayList
+		List<Integer> al = new ArrayList();
+		
+		//Adicionar elementos ao ArrayList
+		al.add(3);
+		al.add(30);
+
+	}
+
+}
