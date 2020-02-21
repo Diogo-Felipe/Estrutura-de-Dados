962255bceb92b9014d45cb7211caf0415f9c8ede
Diogo Felipe <dlcfelipp@gmail.com>
2020-02-21 10:08:02 -0300

    Criada LinkedList

---
+++ src/ED/LinkedList01.java
@@ -0,0 +1,18 @@
+package ED;
+
+import java.util.*;
+
+public class LinkedList01 {
+
+	public static void main(String[] args) {
+		
+		//Criar o LinkedList
+		LinkedList<String> ll = new LinkedList();
+		
+		//Adicionar valores no LinkedList
+		ll.add("Rodrigo");
+		ll.add("Carlos");
+
+	}
+
+}
