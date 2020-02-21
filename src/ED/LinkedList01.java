60bf387225e44a2de8425daeefe5bf2ab353c9ba
Diogo Felipe <dlcfelipp@gmail.com>
2020-02-21 10:24:13 -0300

    Percorrer elementos em uma lista ligada

--- src/ED/LinkedList01.java
+++ src/ED/LinkedList01.java
@@ -7,12 +7,21 @@ public class LinkedList01 {
 	public static void main(String[] args) {
 		
 		//Criar o LinkedList
-		LinkedList<String> ll = new LinkedList();
+		LinkedList<String> ll = new LinkedList<String>();
+		
 		
 		//Adicionar valores no LinkedList
 		ll.add("Rodrigo");
 		ll.add("Carlos");
-
+		
+		
+		//Exibição do elemento usando ITERATOR
+		Iterator<String> iterator = ll.iterator();
+		
+		while(iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+		
 	}
 
 }
