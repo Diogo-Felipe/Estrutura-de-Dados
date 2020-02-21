5a7e12c861ea3d4f3c23c6d04f2e806ad2bb1b31
Diogo Felipe <dlcfelipp@gmail.com>
2020-02-21 10:30:52 -0300

    Criação da classe interna para o exercicio proposto

---
+++ src/ED/CadastroCompras.java
@@ -0,0 +1,18 @@
+package ED;
+
+public class CadastroCompras {
+	
+	//Criar estrutura de produtos
+	//Classe Interna
+	public static class Produto{
+		public String nome;
+		public int quantidade;
+	}
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
