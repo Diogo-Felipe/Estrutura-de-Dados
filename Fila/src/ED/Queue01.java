1647bd9df84b0ec7f6594ad5476370528c8a8645
Diogo Felipe <dlcfelipp@gmail.com>
2020-02-28 10:25:13 -0300

    Adição de comentarios

--- Fila/src/ED/Queue01.java
+++ Fila/src/ED/Queue01.java
@@ -12,17 +12,23 @@ public class Queue01 {
 		filaEspera.add("João");
 		filaEspera.add("Adriana");
 		
+		//Exibe toda fila
 		System.out.println("Elementos da fila: " + filaEspera);
 		
+		//Verifica se a fila está vazia
 		System.out.println("Fila está vazia: " + filaEspera.isEmpty());
 
+		//Verifica o tamanho da fila
 		System.out.println("Tamanho da fila: " + filaEspera.size());
 		
+		//Verifica se existe algum elemento específico na fila
 		String nome = "Adriana";
 		System.out.println("Contem o nome: " + filaEspera.contains(nome));
 		
+		//Pega o primeiro elemento da fila (O que está em iminencia de remção)
 		System.out.println("Nome: " + filaEspera.peek() + " é o primeiro");
 		
+		//Consome o elemento da fila (O primeiro)
 		System.out.println("Removendo da fila " + filaEspera.poll());
 	}
 
