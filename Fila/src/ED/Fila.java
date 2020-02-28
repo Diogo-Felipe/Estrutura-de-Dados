1653b5277879dc3599072e787731ab807d3dc683
Diogo Felipe <dlcfelipp@gmail.com>
2020-02-28 09:03:11 -0300

    Inicialização do Menu

--- Fila/src/ED/Fila.java
+++ Fila/src/ED/Fila.java
@@ -10,6 +10,27 @@ public class Fila {
 	}
 
 	public static void main(String[] args) {
+		
 		Scanner entrada = new Scanner(System.in);
+		
+		//Apontadores de inicio e fim de fila. Armazenam endereços de memória
+		FILA inicio = null;
+		FILA fim = null;
+		
+		FILA aux;
+		
+		int op;
+		
+		do {
+			
+			System.out.println("\nMENU DE OPCOES\n");
+            System.out.println("1 - Inserir na fila");          
+            System.out.println("2 - Consultar toda a fila");
+            System.out.println("3 - Remover da fila");
+            System.out.println("4 - Esvaziar a fila");
+            System.out.println("5 - sair");
+            System.out.print("Digite sua opção");
+			
+		}while(true);
 	}
 }
