e201b555eff0cf65a551f355b5d4e487049b7613
Diogo Felipe <dlcfelipp@gmail.com>
2020-02-28 10:53:44 -0300

    Métodos de exibição do deque

--- Deque/src/ED/DequeEd.java
+++ Deque/src/ED/DequeEd.java
@@ -12,14 +12,29 @@ public class DequeEd {
 		
 		deque.add("Elemento 1 (calda)");
 		deque.addFirst("Elemento 2 (Cabeça)");
-		deque.addLast("Elemento 3 (Calda");
+		deque.addLast("Elemento 3 (Calda)");
 		
 		//Adicionar elementos com retorno de validação (Booleano)
 		deque.offer("Elemento 5 (Calda)");
 		deque.offerFirst("Elemento 6 (Cabeça)");
 		deque.offerLast("Elemento 7 (Calda)");
 		
-
+		
+		//Exibir todo deque
+		System.out.println(deque);
+		
+		//Iterar no deque e exiir elemento por emento
+		//PADRAO (INICIO -> FIM)
+		Iterator iterator = deque.iterator();
+		while(iterator.hasNext()) {
+			System.out.println("\t" + iterator.next());
+		}
+		
+		//REVERSA (FIM -> INICIO)
+		Iterator reverso = deque.descendingIterator();
+		while(iterator.hasNext()) {
+			System.out.println("\t" + reverso.next());
+		}
 	}
 
 }
