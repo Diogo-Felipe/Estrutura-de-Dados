13f4421ff4401e73be8a1bebba7ab44a463882dc
Diogo Felipe <dlcfelipp@gmail.com>
2020-02-28 08:54:04 -0300

    Mudança de estrutura e adição das filas

--- ArrayList and LinkedList/src/ED/CadastroCompras.java
+++ ArrayList and LinkedList/src/ED/CadastroCompras.java
@@ -61,6 +61,7 @@ public class CadastroCompras {
 					}
 					
 				}	
+				
 			} else if (op == 3) {
 				System.out.println("\nRemovendo Produto");
 				
---
+++ Fila/src/ED/Fila.java
@@ -0,0 +1,15 @@
+package ED;
+
+import java.util.Scanner;
+
+public class Fila {
+	
+	private static class FILA{
+		public int num;
+		public FILA prox;
+	}
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+	}
+}
