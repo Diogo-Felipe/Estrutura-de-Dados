df57973a3f65337f21cded5263fedb4f419c90ed
Diogo Felipe <dlcfelipp@gmail.com>
2020-03-06 09:15:13 -0300

    Adição de um novo elemento na pilha

--- Pilha/src/ED/Pilha.java
+++ Pilha/src/ED/Pilha.java
@@ -17,6 +17,35 @@ public class Pilha {
         STACK aux;
             
         int op;
+        
+        do {
+        	
+        	System.out.println("\nMENU DE OPCOES\n");
+            System.out.println("1 - Inserir na pilha");         
+            System.out.println("2 - Consultar toda a pilha");
+            System.out.println("3 - Remover da pilha");
+            System.out.println("4 - Esvaziar a pilha");
+            System.out.println("5 - sair");
+            System.out.print("Digite sua opção");
+            
+            if(op < 1 || op > 5) System.out.println("Opção inválida!");
+            
+            //CRIAR O NO E EMPILHAR (PUSH)
+            if(op == 1){
+            
+             System.out.println("Digite o número a ser inserido na pilha: ");
+             
+             STACK novo = new STACK();
+             novo.num = entrada.nextInt();
+             novo.prox = topo;
+             topo = novo;          
+            
+             System.out.println("Número inserido na pilha");
+            
+            
+            }
+            
+        }while();
 	}
 
 }
