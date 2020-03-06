52e0dfbe9d1daff28cc712b93dc19d76ee48ed97
Diogo Felipe <dlcfelipp@gmail.com>
2020-03-06 09:13:43 -0300

    Inicio do estudo em pilhas

---
+++ Pilha/src/ED/Pilha.java
@@ -0,0 +1,22 @@
+package ED;
+
+import java.util.*;
+
+public class Pilha {
+	
+	private static class STACK{
+		public int num;
+		public STACK prox;
+	}
+
+	public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        STACK topo = null;
+        
+        STACK aux;
+            
+        int op;
+	}
+
+}
