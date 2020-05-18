3f4df3cdcb8e45356819e4066eefd051d90d43bd
Diogo Felipe <dlcfelipp@gmail.com>
2020-05-18 20:04:03 -0300

    Saída de dados para o usuário

--- BubbleSort/src/ED/BubbleSort.java
+++ BubbleSort/src/ED/BubbleSort.java
@@ -6,7 +6,10 @@ public class BubbleSort {
 		int vetor[] = {3,1,4,5,2};
 		int aux = 0;
 		
-
+		System.out.println("Vetor desordenado:");
+		for(int i = 0; i < vetor.length; i++) {
+			System.out.print(vetor[i] + " ");
+		}
 		
 		for(int i = 0; i < vetor.length; i++) {
 			for(int j = 0; j < vetor.length - 1; j++) {
@@ -16,6 +19,11 @@ public class BubbleSort {
 					vetor[j + 1] = aux;
 				}
 			}
+		}
+		
+		System.out.println("\nVetor Ordenado");
+		for(int i = 0; i < vetor.length; i++) {
+			System.out.print(vetor[i] + " ");
 		}
 	}
 
