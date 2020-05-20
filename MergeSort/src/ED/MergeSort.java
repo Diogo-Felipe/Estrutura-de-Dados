f44a87a50601e79c655155241d468f4a531488d7
Diogo Felipe <dlcfelipp@gmail.com>
2020-05-20 16:14:40 -0300

    MergeSort

--- MergeSort/src/ED/MergeSort.java
+++ MergeSort/src/ED/MergeSort.java
@@ -52,6 +52,15 @@ public class MergeSort {
         } 
     } 
   
+    static void mergeSort(int arr[], int l, int r){ 
+        if (l < r){ 
+            int m = (l+r)/2; 
+  
+            mergeSort(arr, l, m); 
+            mergeSort(arr , m+1, r); 
 
+            merge(arr, l, m, r); 
+        } 
+    } 
 
 }
