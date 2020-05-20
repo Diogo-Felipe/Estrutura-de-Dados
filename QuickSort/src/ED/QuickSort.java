5e6b29af3fbe0cce5aaa247af5c8b35cb090c530
Diogo Felipe <dlcfelipp@gmail.com>
2020-05-20 15:53:11 -0300

    função de pártição

--- QuickSort/src/ED/QuickSort.java
+++ QuickSort/src/ED/QuickSort.java
@@ -3,8 +3,33 @@ package ED;
 public class QuickSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
+	
+	int particiona(int vetor[], int menor, int direita){ 
+        int pivo = vetor[direita];  
+        int i = (menor-1); // index of smaller element 
+        for (int j = menor; j < direita; j++) 
+        { 
+            // If current element is smaller than or 
+            // equal to pivot 
+            if (vetor[j] <= pivo) 
+            { 
+                i++; 
+  
+                // swap arr[i] and arr[j] 
+                int temp = vetor[i]; 
+                vetor[i] = vetor[j]; 
+                vetor[j] = temp; 
+            } 
+        } 
+  
+        int aux = vetor[i+1]; 
+        vetor[i+1] = vetor[direita]; 
+        vetor[direita] = aux; 
+  
+        return i+1; 
+    } 
 
 }
