8b5095c89bf7127a542c9ddf4d44783b032a0d0e
Diogo Felipe <dlcfelipp@gmail.com>
2020-05-20 16:02:13 -0300

    Refatoração

--- QuickSort/src/ED/QuickSort.java
+++ QuickSort/src/ED/QuickSort.java
@@ -3,11 +3,23 @@ package ED;
 public class QuickSort {
 
 	public static void main(String[] args) {
+		int vetor[] = {3,1,4,5,2};
 		
-
+		System.out.println("Vetor desordenado:");
+		for(int i = 0; i < vetor.length; i++) {
+			System.out.print(vetor[i] + " ");
+		}
+		
+		quickSort(vetor, 0, vetor.length - 1);
+		
+		System.out.println("\nVetor Ordenado");
+		for(int i = 0; i < vetor.length; i++) {
+			System.out.print(vetor[i] + " ");
+		}
+		
 	}
 	
-	void quickSort(int arr[], int comeco, int fim){ 
+	static void quickSort(int arr[], int comeco, int fim){ 
         if (comeco < fim){ 
             int pi = particiona(arr, comeco, fim); 
   
@@ -16,7 +28,7 @@ public class QuickSort {
         } 
     }
 	
-	int particiona(int vetor[], int comeco, int fim){ 
+	static int particiona(int vetor[], int comeco, int fim){ 
         int pivo = vetor[fim];  
         int i = (comeco-1);
         for (int j = comeco; j < fim; j++) 
